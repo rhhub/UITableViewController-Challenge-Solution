e8f95eee4f9e745e846fe7d927147490c1a26677
rhhub <ryanhuebert@gmail.com>
2014-04-30 12:37:27 -0700

    Solved the challenge.

---
+++ RHTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RHTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by VoidMac on 4/30/14.
+//  Copyright (c) 2014 VoidMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RHTableViewController : UITableViewController
+
+@end
